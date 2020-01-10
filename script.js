84c5346dc792b1759118326176bad257d7516104
Alex Urbini <aurbini92@gmail.com>
2020-01-09 23:35:38 -0500

    goes through questions, stops, and shows score

--- script.js
+++ script.js
@@ -1,114 +1,123 @@
+//Grab elements from HTML 
+var startGameButton = document.getElementById('start-game-button'); 
+var gameArea = document.getElementById('game-area'); 
+var choiceList = document.getElementById('choice-list'); 
+var choices = document.getElementById('choices'); 
+var timer = document.getElementById('time'); 
+
+//starter data 
 var questions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "bigint", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },{
-    title: "What would you use to go over each part of a list",
-    choices: ["loop", "incrementer", "variable", "notation"],
-    answer: "loop"
-    },{
-      title: "What type of data is a true variable",
-      choices: ["`1string", "number", "null", "bool"],
-      answer: "bool"
-      },{
-        title: "What type of data structure has an index.",
-        choices: ["object", "number", "array", "boolean"],
-        answer: "array"
-        } 
-       ]
-      
-      const $ = document.querySelector.bind(document); 
+  {
+    title: "Commonly used data types DO NOT include:",
+  choices: ["strings", "bigint", "alerts", "numbers"],
+  answer: "alerts"
+},
+{
+  title: "The condition in an if / else statement is enclosed within ____.",
+  choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+  answer: "parentheses"
+},{
+title: "What would you use to go over each part of a list",
+choices: ["loop", "incrementer", "variable", "notation"],
+answer: "loop"
+},{
+  title: "What type of data is a true variable",
+  choices: ["`1string", "number", "null", "bool"],
+  answer: "bool"
+  },{
+    title: "What type of data structure has an index.",
+    choices: ["object", "number", "array", "boolean"],
+    answer: "array"
+    } 
+]
 
-      document.querySelector("body").innerHTML += `
-      <div class="header">
-        <div class="score">score = 0</div>
-        <div class="time">time = 0</div> 
-      </div> 
-      <div class="content"> 
-      </div>  
-      
-      
-      <button class="start-button">Start Game</button>
-      `
-      var startButton = document.querySelector(".start-button");
+//global variables 
+var time = 0; 
+var questionTitle = document.createElement('h2')
 
+//index of currrent question 
+var currentQuestion = 0; 
 
-      
+var startGame = function (){
+  //hide the start button
+  startGameButton.style.display = 'none'
+  time = 70; 
+  var timerInterval = setInterval(function(){
+    time--; 
+    timer.textContent = time; 
+    if(time === 0){
+      clearInterval(timerInterval); 
+    }
+  },1000)
+  //display list
+  choiceList.setAttribute('display', 'block'); 
+  choiceList.setAttribute('display', 'block'); 
 
-      document.querySelector('.start-button').addEventListener('click', function(){
-        event.preventDefault(); 
-        var time = 3; 
-        var index = 0; 
-        var timeEl = document.querySelector(".time")
-        // timeEl.innerHTML = `time = ${time}`; 
-        var timerInterval = setInterval(function(){
-          time--; 
-          document.querySelector(".time").innerHTML = `<div>time : ${time}</div>`;
-          //console.log(time);  
-          if(time === 0){
-            clearInterval(timerInterval); 
-          }
-        },1000)
-        getQuestions(time, index, timeEl); 
-        })
+  getQuestion(questionTitle); 
+}
+function getQuestion(questionTitle){
+  //build out the content
+  //console.log(questions[currentQuestion].title)
+  //console.log(currentQuestion)
+  questionTitle.textContent = questions[currentQuestion].title; 
+  //place the h2 in the game area 
+  gameArea.appendChild(questionTitle);
+  //Run through the array of questions 
+  
+  for(var i = 0; i <= questions[currentQuestion].choices.length;i++){
+    //console.log(i); 
+    //onsole.log(questions[currentQuestion].choices.length)
+    //create element for current choice and add content
+    var choice = document.createElement('p');
+    choice.textContent = questions[currentQuestion].choices[i]; 
+    choice.setAttribute('id', `choice${i}`); 
+    choice.setAttribute('value', `${questions[currentQuestion].choices[i]}`); 
+    //place the choice in the game area
+    choices.appendChild(choice); 
+ }
+}
+function choiceChecker(){
+  if(notGameOver()){
+    if(event.target.innerText === questions[currentQuestion].answer){
+      currentQuestion++
 
+      while (choices.firstChild) {
+        choices.removeChild(choices.firstChild);
+      }//console.log('loop end')
+      //choiceList.removeChild(choiceList.lastElementChild); 
+      getQuestion(questionTitle); 
+      }else{
+      //currentQuestion++
+      currentQuestion++
+      while (choices.firstChild) {
+        choices.removeChild(choices.firstChild);
+        }
+      // event.target.innerText = ''
+      //console.log(questionTitle); 
+      //choiceList.removeChild(choiceList.lastElementChild); 
 
-          function getQuestions(time, index, timeEl){
-            //console.log(timeEl); 
-            //console.log(index); 
-            //console.log(time); 
-            startButton.remove(); 
-         
-            document.querySelector('.content').innerHTML = 
-              `
-                <h2>${questions[index].title}</h2>
-              `; 
-              var choice = questions[index].choices;
-              //console.log(choice); 
-              for(i = 0; i < choice.length; i++){
-                document.querySelector(".content").innerHTML += 
-                `<p class = choices-${i} value = ${choice[i]}>${choice[i]} </p>`
+      getQuestion(questionTitle); }
+    }else {
+      timer.remove(); 
+      gameArea.innerHTML = '';
+      var scoreForm = document.createElement('form'); 
+      scoreForm.innerText = time; 
+      gameArea.appendChild(scoreForm);  
+    }
+}
 
-            }
-            //console.log(index); 
-            pickedChoice(i, index,time); 
-          }
-
-        function pickedChoice(i, index, time){
-          //console.log(questions[index].answer); 
-          var aChoice1 = document.querySelector(`.choices-1`); 
-          var aChoice2 = document.querySelector(`.choices-2`); 
-          var aChoice3 = document.querySelector(`.choices-3`); 
-          var aChoice4 = document.querySelector(`.choices-4`); 
-
-
-          aChoice1.addEventListener("click",function(){
-            // console.log("bla")
-            // console.log(aChoice1.getAttribute('value'))
-
-          //  console.log(questions);
-             console.log(index); 
-             if(aChoice1.getAttribute('value') === questions[index].answer){
-              console.log("bla") 
-              index++; 
-              console.log("index", index)
-              getQuestions(i, index,null)
-             }else{
-              console.log(index);
-              index++; 
-              getQuestions(i, index,null)
-
-              //console.log("bla")
-               //time = time - 15; 
-              // getQuestions(index, null); 
-
-             }
-          
-         })
-      }
+function notGameOver(){
+  //console.log(currentQuestion); 
+  //console.log(questions.length); 
+  if(currentQuestion === questions.length -1){
+    return false; 
+  }
+  else if (currentQuestion < questions.length- 1){
+    return true; 
+  }else{
+    return false; 
+}
+}
+ //Events or user inputs
+ startGameButton.addEventListener('click', startGame); 
+ choices.addEventListener('click',choiceChecker); 
--- script2.js
+++ script2.js
@@ -1,11 +1,156 @@
+/*
+VERSION 2 
+
+/*
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "bigint", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },{
+    title: "What would you use to go over each part of a list",
+    choices: ["loop", "incrementer", "variable", "notation"],
+    answer: "loop"
+    },{
+      title: "What type of data is a true variable",
+      choices: ["`1string", "number", "null", "bool"],
+      answer: "bool"
+      },{
+        title: "What type of data structure has an index.",
+        choices: ["object", "number", "array", "boolean"],
+        answer: "array"
+        } 
+       ]
+      
+      const $ = document.querySelector.bind(document); 
+
+      document.querySelector("body").innerHTML += `
+      <div class="header">
+        <div class="score">score = 0</div>
+        <div class="time">time = 0</div> 
+      </div> 
+      <div class="content"> 
+      </div>  
+      
+      
+      <button class="start-button">Start Game</button>
+      `
+      var startButton = document.querySelector(".start-button");
+
+
+      
+
+      document.querySelector('.start-button').addEventListener('click', function(){
+        event.preventDefault(); 
+        var time = 3; 
+        var index = 0; 
+        var timeEl = document.querySelector(".time")
+        // timeEl.innerHTML = `time = ${time}`; 
+        var timerInterval = setInterval(function(){
+          time--; 
+          document.querySelector(".time").innerHTML = `<div>time : ${time}</div>`;
+          //console.log(time);  
+          if(time === 0){
+            clearInterval(timerInterval); 
+          }
+        },1000)
+        getQuestions(time, index, timeEl); 
+        })
+
+
+          function getQuestions(time, index, timeEl){
+            //console.log(timeEl); 
+            //console.log(index); 
+            //console.log(time); 
+            startButton.remove(); 
+         
+            document.querySelector('.content').innerHTML = 
+              `
+                <h2>${questions[index].title}</h2>
+              `; 
+              var choice = questions[index].choices;
+              //console.log(choice); 
+              for(i = 0; i < choice.length; i++){
+                document.querySelector(".content").innerHTML += 
+                `<p class = choices-${i} value = ${choice[i]}>${choice[i]} </p>`
+                
+            }
+            //console.log(index); 
+            pickedChoice(i, index,time); 
+          }
+
+        function pickedChoice(i, index, time){
+          //console.log(questions[index].answer); 
+          var aChoice1 = document.querySelector(`.choices-0`); 
+          var aChoice2 = document.querySelector(`.choices-1`); 
+          var aChoice3 = document.querySelector(`.choices-2`); 
+          var aChoice4 = document.querySelector(`.choices-3`); 
+
+        
+          aChoice1.addEventListener("click",function(){ 
+             console.log(index); 
+             if(aChoice1.getAttribute('value') === questions[index].answer){
+              index++; 
+              getQuestions(i, index,null)
+             }else{
+              index++; 
+              getQuestions(i, index,null)
+             } 
+         })
+      aChoice2.addEventListener("click",function(){
+         console.log(index); 
+         if(aChoice2.getAttribute('value') === questions[index].answer){
+          index++; 
+          getQuestions(i, index,null)
+         }else{
+          index++; 
+          getQuestions(i, index,null)
+         }
+      })
+  
+  aChoice3.addEventListener("click",function(){
+     console.log(index); 
+     if(aChoice3.getAttribute('value') === questions[index].answer){
+      index++; 
+      getQuestions(i, index,null)
+     }else{
+      index++; 
+      getQuestions(i, index,null)
+      }
+    })
+aChoice4.addEventListener("click",function(){
+   console.log(index); 
+   if(aChoice4.getAttribute('value') === questions[index].answer){
+    index++; 
+    getQuestions(i, index,null)
+   }else{
+    index++; 
+    getQuestions(i, index,null)
+    }
+  })
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 /*
 
-
-
+VERSION 1 
   
           setting base variables
     var score = 0; 
